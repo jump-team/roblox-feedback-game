4a7991133ad284f72c3806fe09cba8f608075b4a
Dog2puppy <colecooper2005@icloud.com>
2018-12-26 22:02:36 -0500

    Add approvedUser config option
    Defaults to the game creator

--- src/server/config.ts
+++ src/server/config.ts
@@ -1,2 +1,3 @@
 export const baseUrl = "https://roblox-feedback.glitch.me"; // Replace with your own instance of Roblox feedback
 export const secretToken = "JumpGameFeedback"; // Secret token from when you setup the server
+export const approvedUser = game.CreatorId; // Approved user for viewing all feedback. 
