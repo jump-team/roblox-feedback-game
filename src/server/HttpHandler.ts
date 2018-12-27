2ce5b4bca6cf54c5e9e7a376abaaff6789ccb5a1
Dog2puppy <colecooper2005@icloud.com>
2018-12-27 09:55:17 -0500

    Setup tslint

--- src/server/HttpHandler.ts
+++ src/server/HttpHandler.ts
@@ -1,94 +1,95 @@
-import { HttpService } from "rbx-services";
-import { baseUrl, secretToken } from "./config";
+import { HttpService } from "rbx-services"
+import { baseUrl, secretToken } from "./config"
 
 function makeHttpServiceCall(options: RequestAsyncRequest): RequestAsyncResponse {
-	let pcallResponse = pcall(function() {
-		let response = HttpService.RequestAsync(options);
-		return response;
-	});
+	const pcallResponse = pcall(function() {
+		const response = HttpService.RequestAsync(options)
+		return response
+	})
 	if (pcallResponse[0]) {
-		const [,httpResponse] = pcallResponse;
+		const [, httpResponse] = pcallResponse
 		if (httpResponse.StatusCode !== 200) {
-			throw "Status code is not 200.";
-		}else{
-			return httpResponse;
+			throw "Status code is not 200."
+		} else {
+			return httpResponse
 		}
-	}else{
-		throw `Request failed: ${pcallResponse[1]}`;
+	} else {
+		throw `Request failed: ${pcallResponse[1]}`
 	}
 }
 
 export namespace HttpHandler {
-	export function getAllFeedback() {
-		let pcallResponse = pcall(function() {
-			let response = makeHttpServiceCall({ Url: baseUrl + "/feedback" })
-			return response;
-		});
+	export function getAllFeedback(): [boolean, object | string] {
+		const pcallResponse = pcall(function() {
+			const response = makeHttpServiceCall({ Url: baseUrl + "/feedback" })
+			return response
+		})
 		if (pcallResponse[0]) {
-			const [,feedback] = pcallResponse;
-			return [true, HttpService.JSONDecode(feedback.Body)];
-		}else{
-			return [false, pcallResponse[1]];
+			const [, feedback] = pcallResponse
+			return [true, HttpService.JSONDecode(feedback.Body) as object]
+		} else {
+			const [, response] = pcallResponse
+			return [false, response]
 		}
 	}
-	export function getMyFeedback(playerId: number) {
-		let pcallResponse = pcall(function() {
-			let response = makeHttpServiceCall({ Url: baseUrl + `/feedback/${playerId}` })
-			return response;
-		});
+	export function getMyFeedback(playerId: number): [boolean, object | string] {
+		const pcallResponse = pcall(function() {
+			const response = makeHttpServiceCall({ Url: baseUrl + `/feedback/${playerId}` })
+			return response
+		})
 		if (pcallResponse[0]) {
-			const [,feedback] = pcallResponse;
-			return [true, HttpService.JSONDecode(feedback.Body)];
-		}else{
-			return [false, pcallResponse[1]];
+			const [, feedback] = pcallResponse
+			return [true, HttpService.JSONDecode(feedback.Body) as object]
+		} else {
+			return [false, pcallResponse[1]]
 		}
 	}
-	export function postFeedback(subject: string, author: number, body: string) {
-		let object = {
-			subject: subject,
-			author: author,
-			body: body
+	export function postFeedback(subject: string, author: number, body: string): [boolean, string] {
+		const object = {
+			subject,
+			author,
+			body,
 		}
-		let httpBody = HttpService.JSONEncode(object)
-		let httpOptions: RequestAsyncRequest = {
+		const httpBody = HttpService.JSONEncode(object)
+		const httpOptions: RequestAsyncRequest = {
 			Url: baseUrl + "/feedback",
 			Method: "POST",
 			Headers: { Authorization: "Bearer" + secretToken },
-			Body: httpBody
+			Body: httpBody,
 		}
-		let pcallResponse = pcall(function() {
-			let response = makeHttpServiceCall(httpOptions);
-			return response;
-		});
+		const pcallResponse = pcall(function() {
+			const response = makeHttpServiceCall(httpOptions)
+			return response
+		})
 		if (pcallResponse[0]) {
-			const [,response] = pcallResponse;
+			const [, response] = pcallResponse
 			return [true, response.Body]
-		}else{
-			return [false, pcallResponse[1]];
+		} else {
+			return [false, pcallResponse[1]]
 		}
 	}
-	export function postFeedbackComment(feedbackId: string, subject: string, author: number, body: string) {
-		let object = {
-			subject: subject,
-			author: author,
-			body: body
+	export function postFeedbackComment(feedbackId: string, subject: string, author: number, body: string): [boolean, string] {
+		const object = {
+			subject,
+			author,
+			body,
 		}
-		let httpBody = HttpService.JSONEncode(object)
-		let httpOptions: RequestAsyncRequest = {
+		const httpBody = HttpService.JSONEncode(object)
+		const httpOptions: RequestAsyncRequest = {
 			Url: baseUrl + `/feedback/${feedbackId}/comment`,
 			Method: "POST",
 			Headers: { Authorization: "Bearer" + secretToken },
-			Body: httpBody
+			Body: httpBody,
 		}
-		let pcallResponse = pcall(function() {
-			let response = makeHttpServiceCall(httpOptions);
-			return response;
-		});
+		const pcallResponse = pcall(function() {
+			const response = makeHttpServiceCall(httpOptions)
+			return response
+		})
 		if (pcallResponse[0]) {
-			const [,response] = pcallResponse;
+			const [, response] = pcallResponse
 			return [true, response.Body]
-		}else{
-			return [false, pcallResponse[1]];
+		} else {
+			return [false, pcallResponse[1]]
 		}
 	}
 }
--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -1,12 +1,18 @@
-import { HttpHandler } from "./HttpHandler";
-import { baseUrl, approvedUser } from "./config";
-import Net from 'rbx-net';
+import Net from "rbx-net"
+import { approvedUser, baseUrl } from "./config"
+import { HttpHandler } from "./HttpHandler"
 
-let getAllFeedbackRemote = new Net.ServerFunction("GetAllFeedback");
-let getMyFeedbackRemote = new Net.ServerFunction("GetMyFeedback");
-let postFeedbackRemote = new Net.ServerFunction("PostFeedback");
-let postFeedbackCommentRemote = new Net.ServerFunction("PostFeedbackComment");
+const getAllFeedbackRemote = new Net.ServerFunction("GetAllFeedback")
+const getMyFeedbackRemote = new Net.ServerFunction("GetMyFeedback")
+const postFeedbackRemote = new Net.ServerFunction("PostFeedback")
+const postFeedbackCommentRemote = new Net.ServerFunction("PostFeedbackComment")
 
 getAllFeedbackRemote.Callback = () => {
-	
+	return HttpHandler.getAllFeedback()
+}
+
+getMyFeedbackRemote.ClientCache = 15
+getMyFeedbackRemote.Callback = (player: Player) => {
+	const id = player.UserId
+
 }
